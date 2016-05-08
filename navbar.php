3d8bbc375af0706297b28492f326ef9b45804f66
keatinge <willy.keatinge@gmail.com>
2016-05-08 18:50:34 -0400

    global navbar for site

---
+++ navbar.php
@@ -0,0 +1,7 @@
+<div class="well" style="margin-top:5px;margin-bottom:5px;">
+    <center>
+         <a class="btn btn-default" href="/paster/index.php">Create new paste</a>
+
+    </center>
+</div>
+
