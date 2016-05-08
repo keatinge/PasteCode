087365983f9efbdf309986ca5a17ab738749ff13
keatinge <willy.keatinge@gmail.com>
2016-05-08 19:10:42 -0400

    add example settings

---
+++ exampleSettings.php
@@ -0,0 +1,14 @@
+<?php
+//rename this file to settings.php when you are done
+
+$pdoHost = "localhost";
+$pdoUsername = "";
+$pdoPassword = "";
+$pdoDatabase = "";
+$pdo = new PDO("mysql:host=$pdoHost;dbname=$pdoDatabase", $pdoUsername, $pdoPassword);
+
+
+//url that you are hosting the website on
+$baseUrl = "";
+
+?>
