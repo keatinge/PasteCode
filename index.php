2723b8c02775f8a91ca4618f3a1eda042d3ce1b2
keatinge <willy.keatinge@gmail.com>
2016-05-08 18:50:22 -0400

    added posting contents with form

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 
@@ -29,6 +30,13 @@
         .invis {
             visibility: hidden;
         }
+        
+        .ajaxAlert {
+            margin-bottom: 5px;
+            margin-top: 20px;
+            text-align: center;
+            font-weight: bold;
+        }
 
 
     </style>
@@ -46,35 +54,35 @@
         </div>
     </div>
 
+    <div id="alertBox">
 
-    <div class="well main-container invis" id="config" >
+    </div>
+
+
+    <div class="well main-container invis" id="config">
         <h1 style="text-align:center;">Configure your settings for this paste</h1>
         <div class="options">
             <center>
 
-                <label>Visibility</label>
-                <select class="form-control sel-opt" id="vis">
-                    <option>Public</option>
-                    <option>Unlisted</option>
-                    <option>One Time View</option>
-                </select>
-                <br>
+                <form action="submitPaste.php" method="post">
 
-                <label style="text-align:left;">Title</label>
-                <input type="text" placeholder="Untitled Project" class="form-control sel-opt" id="title">
-                <br>
+                    <label style="text-align:left;">Title</label>
+                    <input type="text" name="title" placeholder="Untitled Project" class="form-control sel-opt" id="title">
+                    <br>
 
-                <label style="text-align:left;">Author</label>
-                <input type="text" placeholder="Anonymous" class="form-control sel-opt" id="author">
-                <br>
+                    <label style="text-align:left;">Author</label>
+                    <input type="text" placeholder="Anonymous" class="form-control sel-opt" name="author" id="author">
+                    <br>
 
-                <label style="text-align:left;">Deletion Password</label>
-                <input type="password" placeholder="no deletion password" class="form-control sel-opt" id="deletepw">
-                <br>
+                    <label style="text-align:left;">Deletion Password</label>
+                    <input type="password" placeholder="no deletion password" class="form-control sel-opt" name="deletepw" id="deletepw">
 
+                    <input type="hidden" id="pasteContents" name="pasteContents">
+                    <br>
 
-                <button style="width:20%" id="goBack" class="btn btn-danger"><-- Go Back</button>
-                <button style="width:20%" id="finalSubmit" class="btn btn-success">Submit</button>
+                    <button style="width:20%" id="goBack" class="btn btn-danger"><-- Go Back</button>
+                    <input type="submit" style="width:20%" id="finalSubmit" value="Submit" class="btn btn-success">
+                </form>
 
             </center>
         </div>
@@ -101,67 +109,15 @@
             $("#pasteInput").slideUp(400);
             $("#config").removeClass("invis");
 
+            $("#pasteContents").val(pastedText);
+
         } else {
             alert("There's something wrong with your paste!");
         }
 
     });
 
-    $("#config").on("click", "#finalSubmit", function () {
-
-
-        var visibility = $("#vis").val();
-        var title = $("#title").val();
-        var author = $("#author").val();
-        var deletionPassword = $("#deletepw").val();
-
-
-        console.log(pastedText);
-        console.log(visibility);
-        console.log(title);
-        console.log(author);
-        console.log(deletionPassword);
-
-        var urlString = "submitPaste.php";
-
-        var payload = {
-            text : pastedText,
-            vis : visibility,
-            auth : author,
-            delpw : deletionPassword
-        };
-
-
-        var post = $.post(urlString, payload);
-
-        post.done(function (data) {
-            response = data;
-
-            //workaround for  php $HTTP_RAW_POST_DATA warning
-            var re = /!!!!!(.+)/;
-            response = re.exec(response)[1]
-
-
-            alert(response)
-
-
-        });
-
-
-
-
-
-
-    });
-
 
 
 </script>
-
-
-
-
-
-
-
 </html>
